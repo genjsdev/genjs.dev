c7f389330cdfb75e0bbf6927ae6e3a4cf5f9c9f0
Olivier Hoareau <oha@greenberets.io>
2020-07-29 23:18:24 +0200

    chore: url for production

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://genjs.dev',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
